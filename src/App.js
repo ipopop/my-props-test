a0d9e38193f6e38d74cf67ea854f667cfa677e7a
popdev <popdev@Mini.local>
2020-06-16 11:40:02 +0200

    comsetic's vers.4

--- src/App.js
+++ src/App.js
@@ -6,38 +6,44 @@ import './css/App.css';
 function App() {
   return (
     <>
-      <img className="logo" src="logo512.png" alt="React Logo"/>
-      <div class="title">
-        <h3><span>Props</span> tests on <span className="color1">React</span></h3>
-      </div>
-      <div className="container">
-      <div className="card">
+      <content>
+        <img className="logo" src="./img/logo512.png" alt="React Logo"/>
+        <div class="title">
+          <h3><span>Props</span> tests on <span className="color1">React</span></h3>
+        </div>
+        <div className="container">
+        <div className="card">
+          <Hello
+            to="Ringo"
+            from="Paul"
+            num={3}
+            data={[1,2,3,4,5]}
+            isFunny={false}
+            src="./img/chutter-snapQGGflC3gIzU-unsplash.jpg"
+            alt="img1 from Unsplash"
+          />
+        </div>
+        <div className="card">
         <Hello
-          to="Ringo"
-          from="Paul"
-          num={3}
-          data={[1,2,3,4,5]}
-          isFunny={false}
-          src="chutter-snapQGGflC3gIzU-unsplash.jpg"
-          alt="img1 from Unsplash"
+          to="John"
+          from="George"
+          num={4}
+          data={[6,7,8,9,10]}
+          isFunny
+          src="./img/trevormckinnon-jqdJ0A0lNV4-unsplash.jpg"
+          alt="img2 from Unsplash"
         />
-      </div>
-      <div className="card">
-      <Hello
-        to="John"
-        from="George"
-        num={4}
-        data={[6,7,8,9,10]}
-        isFunny
-        src="trevormckinnon-jqdJ0A0lNV4-unsplash.jpg"
-        alt="img2 from Unsplash"
-      />
-      </div>
-      </div>
-      <div class="title">
-        <h3><span className="color1">Charts Components</span> with <span>Props</span></h3>
-      </div><br/>
-      <Charts />
+        </div>
+        </div>
+        <div class="title">
+          <h3><span className="color1">Charts Components</span> with <span>Props</span></h3>
+        </div><br/>
+        <Charts />
+    </content>
+    <footer>
+      <p>Inspired by "The_Modern_React_Bootcamp" course by Colt Steel</p>
+      <p>iPopop © 2020</p>
+    </footer>
     </>
   );
 }

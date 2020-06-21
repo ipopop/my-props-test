a6765ef49a050d4be6bb4e983caef23859989f69
popdev <popdev@Mini.local>
2020-06-21 11:48:17 +0200

    add img placehoder & emojis on 'artists list' section

--- src/App.js
+++ src/App.js
@@ -1,97 +1,107 @@
-import React from 'react';
-import Hello from './components/Hello';
-import Charts from './components/Charts';
-import Artist from './components/ArtistList';
-import SlotMachine from './components/slotMachine';
-import './css/App.css';
-import './css/animTxt.css';
+import React from "react";
+import Hello from "./components/Hello";
+import Charts from "./components/Charts";
+import Artist from "./components/ArtistList";
+import SlotMachine from "./components/slotMachine";
+import "./css/App.css";
+import "./css/animTxt.css";
 
 function App() {
   return (
     <>
       <content>
-        <a href="https://my-props-test.vercel.app/"><img className="logo" src="./img/logo512.png" alt="React Logo"/></a>
-        <div class="title">
-          <h3>My firsts <span>Props</span> tests on <span className="color1">React</span></h3>
+        <a href='https://my-props-test.vercel.app/'>
+          <img className='logo' src='./img/logo512.png' alt='React Logo' />
+        </a>
+        <div class='title'>
+          <h3>
+            My firsts <span>Props</span> tests on{" "}
+            <span className='color1'>React</span>
+          </h3>
         </div>
-        <div className="container">
-        <div className="card">
-          <Hello
-            to="Ringo"
-            from="Paul"
-            num={3}
-            data={[1,2,3,4,5]}
-            isFunny={false}
-            src="./img/chutter-snapQGGflC3gIzU-unsplash.jpg"
-            alt="img1 from Unsplash"
-          />
+        <div className='container'>
+          <div className='card'>
+            <Hello
+              to='Ringo'
+              from='Paul'
+              num={3}
+              data={[1, 2, 3, 4, 5]}
+              isFunny={false}
+              src='./img/chutter-snapQGGflC3gIzU-unsplash.jpg'
+              alt='img1 from Unsplash'
+            />
+          </div>
+          <div className='card'>
+            <Hello
+              to='John'
+              from='George'
+              num={4}
+              data={[6, 7, 8, 9, 10]}
+              isFunny
+              src='./img/trevormckinnon-jqdJ0A0lNV4-unsplash.jpg'
+              alt='img2 from Unsplash'
+            />
+          </div>
         </div>
-        <div className="card">
-        <Hello
-          to="John"
-          from="George"
-          num={4}
-          data={[6,7,8,9,10]}
-          isFunny
-          src="./img/trevormckinnon-jqdJ0A0lNV4-unsplash.jpg"
-          alt="img2 from Unsplash"
-        />
+        <div class='title'>
+          <h3>
+            Artists List (<span className='color1'>React</span>{" "}
+            <span className='color2'>Components</span> with <span>Props</span>)
+          </h3>
         </div>
+        <div className='container artist'>
+          <div className='card-2'>
+            <Artist
+              name='John Lennon'
+              job={["Singer", "Composer", "Musician"]}
+            />
+          </div>
+          <div className='card-2'>
+            <Artist
+              name='Jodie Foster'
+              job={["Actress", "Film director", "Producer"]}
+            />
+          </div>
         </div>
-        <div class="title">
-          <h3>Various <span className="color2">Charts Components</span><br/>with inner <span>Props</span> on <span className="color1">React</span></h3>
-        </div><br/>
+        <div class='title'>
+          <h3>
+            Slot Machine (<span className='color1'>React</span>{" "}
+            <span className='color2'>Components</span> with <span>Props</span>)
+          </h3>
+        </div>
+        <div className='container'>
+          <div className='card-2'>
+            <SlotMachine s1='🍊' s2='🍉' s3='🍓' />
+          </div>
+          <div className='card-2'>
+            <SlotMachine s1='🍐' s2='🍎' s3='🍏' />
+          </div>
+          <div className='card-2'>
+            <SlotMachine s1='🍒' s2='🍒' s3='🍒' />
+          </div>
+        </div>
+        <div class='title'>
+          <h3>
+            Various <span className='color2'>Charts Components</span>
+            <br />
+            with inner <span>Props</span> on{" "}
+            <span className='color1'>React</span>
+          </h3>
+        </div>
+        <br />
         <Charts />
-        <div class="title">
-          <h3>Artists List (<span className="color1">React</span> <span className="color2">Components</span> with <span>Props</span>)</h3>
+      </content>
+      <footer>
+        <div class='typewriter'>
+          <p>
+            <em>
+              "Freely inspired by the begining of 'The Modern React Bootcamp'
+              course by Colt Steel"
+            </em>
+          </p>
         </div>
-        <div className="container">
-          <div className="card-2">
-          <Artist
-            name="John Lennon"
-            job={["Singer","Composer","Musician"]}
-          />
-          </div>
-          <div className="card-2">
-          <Artist
-            name="Jodie Foster"
-            job={["Actress","Film director","Producer"]}
-          />
-          </div>
-        </div>
-        <div class="title">
-          <h3>Slot Machine (<span className="color1">React</span> <span className="color2">Components</span> with <span>Props</span>)</h3>
-        </div>
-        <div className="container">
-          <div className="card-2">
-          <SlotMachine
-            s1="🍊"
-            s2="🍉"
-            s3="🍓"
-          />
-          </div>
-          <div className="card-2">
-          <SlotMachine
-            s1="🍐"
-            s2="🍎"
-            s3="🍏"
-          />
-          </div>
-          <div className="card-2">
-          <SlotMachine
-            s1="🍒"
-            s2="🍒"
-            s3="🍒"
-          />
-          </div>
-        </div>
-    </content>
-    <footer>
-      <div class="typewriter">
-        <p><em>"Freely inspired by the begining of 'The Modern React Bootcamp' course by Colt Steel"</em></p>
-      </div>
-      <p>iPopop © 2020</p>
-    </footer>
+        <p>iPopop © 2020</p>
+      </footer>
     </>
   );
 }
--- src/components/ArtistList.jsx
+++ src/components/ArtistList.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import './../css/artistList.css';
 
 class Artist extends React.Component {
+    static defaultProps = {
+        src: 'https://dummyimage.com/150x100.jpg',
+        alt: ''
+    }
     render() {
-        const { name, job } = this.props;
+        const { name, job, src, alt } = this.props;
         return <div>
-            <p>Artist Name :</p>
+            <img src={src} alt={alt}/>
             <p><span>{name}</span></p>
-            <ul>
+            <ul className="single-before">
                 {job.map(h =>
                 <li>
                     <span>{h}</span>
